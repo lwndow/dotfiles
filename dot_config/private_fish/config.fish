0c21fa94a52fd2a61bd3b2ea751eb6ae77f1049a
lwndow <lwndow@users.noreply.github.com>
2023-04-02 12:15:24 -0500

    adding fish files

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,27 @@
+if status is-interactive
+    # homebrew
+    if test -x /opt/homebrew/bin/brew
+        /opt/homebrew/bin/brew shellenv | source
+    end
+
+    # pyenv
+    if test -x /Users/mitch/.pyenv/bin/pyenv
+        set -x PYENV_ROOT $HOME/.pyenv
+        set --prepend PATH $PYENV_ROOT/bin
+        pyenv init - | source
+        pyenv virtualenv-init - | source
+    end
+
+    # subl command
+    set --prepend PATH "/Applications/Sublime Text.app/Contents/SharedSupport/bin"
+
+    # zoxide
+    if test -x /opt/homebrew/bin/zoxide
+        zoxide init fish | source
+    end    
+
+    # starship
+    if test -x /opt/homebrew/bin/starship
+        starship init fish | source
+    end
+end
