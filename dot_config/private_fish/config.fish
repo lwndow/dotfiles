c8a34d037ccb144fdd295a2b3dc5bb8dc6cd02f2
lwndow <lwndow@users.noreply.github.com>
2023-06-16 10:09:17 -0500

    fisher update (probably should treat these as externals) + path updates

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,11 +1,23 @@
 if status is-interactive
+    # which OS?
+    switch (uname)
+    case Darwin
+        set starship_bin_path /opt/homebrew/bin/starship
+        set zoxide_bin_path /opt/homebrew/bin/zoxide
+        set subl_test_path "/Applications/Sublime Text.app/Contents/SharedSupport/bin"
+    case Linux
+        set starship_bin_path /usr/bin/starship
+        set zoxide_bin_path /usr/bin/zoxide
+        set subl_test_path /usr/bin/subl
+    end
+
     # homebrew
     if test -x /opt/homebrew/bin/brew
         /opt/homebrew/bin/brew shellenv | source
     end
 
     # pyenv
-    if test -x /Users/mitch/.pyenv/bin/pyenv
+    if test -x $HOME/.pyenv/bin/pyenv
         set -x PYENV_ROOT $HOME/.pyenv
         set --prepend PATH $PYENV_ROOT/bin
         pyenv init - | source
@@ -13,18 +25,21 @@ if status is-interactive
     end
 
     # subl command
-    if test -d "/Applications/Sublime Text.app/Contents/SharedSupport/bin"
-        set --prepend PATH "/Applications/Sublime Text.app/Contents/SharedSupport/bin"
+    if test -d $subl_test_path
+        set --prepend PATH $subl_test_path
+        set -x EDITOR "subl"
+    end
+    if test -x $subl_test_path
         set -x EDITOR "subl"
     end
 
     # zoxide
-    if test -x /opt/homebrew/bin/zoxide
+    if test -x $zoxide_bin_path
         zoxide init fish | source
     end    
 
     # starship
-    if test -x /opt/homebrew/bin/starship
+    if test -x $starship_bin_path
         starship init fish | source
     end
 end
